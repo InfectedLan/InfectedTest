a2a324d2d4e24e956fa13ffded2f4c48e4c00e89
Liam Svanåsbakken Crouch <spam@petterroea.com>
2015-05-21 22:49:55 +0200

    more tests

--- tests/ClanTestSuite.php
+++ tests/ClanTestSuite.php
@@ -20,7 +20,7 @@ require_once 'objects/user.php';
 class ClanTestSuite extends TestSuite {
 	public function test() {
 		$this->clanCreationTest();
-		//$this->clanInviteMemberTest();
+		$this->clanInviteMemberTest();
 	}
 
 	/*
@@ -31,15 +31,17 @@ class ClanTestSuite extends TestSuite {
 		$user = UserHandler::getUserByIdentifier("petterroea"); //Any user we know is in the db
 		$testUser = UserHandler::getUserByIdentifier("test1"); //Another user. This one shouldnt be admin, to test permission stuff
 
-		// TODO: Petterroea, this is wrong...
-		//$clan = ClanHandler::getClansByUser($user);
-		//$members = $clan->getMembers();
-		//$this->assert_equals(count($members), 1);
-		
-		// Invite a new member. We have to go through the entire registration process because api.
-		//$clan->invite($testUser);
-		//$invites = InviteHandler::getInvitesByUser($testUser);
-		//$this->assert_equals(count($invites), 1);
+		//ClanHandler::getClansByUser()
+		if($this->assert_equals(count(ClanHandler::getClansByUser($user)), 1)) {
+			$clan = ClanHandler::getClansByUser($user)[0];
+			$members = $clan->getMembers();
+			$this->assert_equals(count($members), 1);
+			
+			// Invite a new member. We have to go through the entire registration process because api.
+			$clan->invite($testUser);
+			$invites = InviteHandler::getInvitesByUser($testUser);
+			$this->assert_equals(count($invites), 1);
+		}
 	}
 
 	private function clanCreationTest() {
@@ -56,15 +58,34 @@ class ClanTestSuite extends TestSuite {
 		
 		$compo = CompoHandler::getCompo($sql->insert_id);
 
+		$this->assert_equals(count(ClanHandler::getClansByCompo($compo)), 0);
+
 		if ($this->assert_not_equals($event, null)) {
 			$clan = ClanHandler::createClan($event, "Test clan", "CLAN", $compo, $user);
 			
 			if ($this->assert_not_equals($clan, null)) {
+				//ClanHandler::getClansByCompo()
+				$this->assert_equals(count(ClanHandler::getClansByCompo($compo)), 1);
+
 				$this->assert_equals($clan->getName(), "Test clan");
 				$this->assert_equals($clan->getTag(), "CLAN");
 				$this->assert_equals($clan->getChief()->getId(), $user->getId());
 				$this->assert_equals($clan->getCompo()->getId(), $compo->getId());
 				$members = $clan->getMembers();
+				
+				if ($this->assert_equals(count($members), 1)) {
+					$this->assert_equals($members[0]->getId(), $user->getId());
+				}
+
+				//ClanHandler::getClan()
+				$fetchClanTest = ClanHandler::getClan($clan->getId());
+				$this->assert_equals($fetchClanTest->getId(), $clan->getId());
+
+				$this->assert_equals($fetchClanTest->getName(), "Test clan");
+				$this->assert_equals($fetchClanTest->getTag(), "CLAN");
+				$this->assert_equals($fetchClanTest->getChief()->getId(), $user->getId());
+				$this->assert_equals($fetchClanTest->getCompo()->getId(), $compo->getId());
+				$members = $fetchClanTest->getMembers();
 				
 				if ($this->assert_equals(count($members), 1)) {
 					$this->assert_equals($members[0]->getId(), $user->getId());
